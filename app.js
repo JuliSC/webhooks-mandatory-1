d25d70efab95b2fdfbb64590ba6ade339e919dcd
Julius Stenbæk Christoffersen <julius@adnudging.com>
2023-03-26 22:50:31 +0200

    moved endpoints back to app.js for ease of working with webhooks arr

--- app.js
+++ app.js
@@ -5,10 +5,17 @@ import swaggerUi from "swagger-ui-express";
 import swaggerJSDoc from "swagger-jsdoc";
 import fs from "fs";
 
-import webhooksRouter from "./routes/webhookRouter.js";
-import eventsRouter from "./routes/eventRouter.js";
+// import webhooksRouter from "./routes/webhookRouter.js";
+// import eventsRouter from "./routes/eventRouter.js";
+
+import {Router} from "express";
+
+const router = Router();
 
 const PORT = process.env.PORT || 8080;
+
+const file = fetchFile("./webhooks.json");
+let webhooks = JSON.parse(file);
 
 // Create the express app
 const app = express();
@@ -38,11 +45,142 @@ app.use(
   swaggerUi.setup(swaggerJSDoc(swaggerOptions))
 );
 
-app.use("/webhooks", webhooksRouter);
-app.use("/events", eventsRouter);
+// Webhooks router
+// Route to register a webhook
+/**
+ * @openapi
+ * /webhooks/register:
+ *   post:
+ *     description: Registers a new webhook for a provided endpoint and event.
+ *     requestBody:
+ *       description: Endpoint to register. And which event to register it for. NOTICE - eventType CAN ONLY BE alice OR bob.
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               endpoint:
+ *                 type: string
+ *               eventType:
+ *                 type: string
+ *                 enum: [bob, alice]
+ *             required:
+ *               - endpoint
+ *               - eventType
+ *           example:
+ *             endpoint: http://example.com/your-endpoint
+ *             eventType: bob
+ *     responses:
+ *       200:
+ *         description: Returns confirmation msg.
+ */
+router.post("/webhooks/register", (req, res) => {
+  const {eventType, endpoint} = req.body;
+  webhooks.push({eventType, endpoint});
+  saveWebhooks();
+  res.send("Webhook registered successfully");
+});
 
-const file = fetchFile("./webhooks.json");
-let webhooks = JSON.parse(file);
+// Route to unregister a webhook
+/**
+ * @openapi
+ * /webhooks/unregister:
+ *   post:
+ *     description: Unregisters all webhooks created with the provided endpoint.
+ *     requestBody:
+ *       description: Endpoint to unregister.
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               endpoint:
+ *                 type: string
+ *             required:
+ *               - endpoint
+ *           example:
+ *             endpoint: http://example.com/your-endpoint
+ *     responses:
+ *       200:
+ *         description: Returns confirmation msg.
+ */
+router.post("/webhooks/unregister", (req, res) => {
+  const {endpoint} = req.body;
+  webhooks = webhooks.filter((webhook) => webhook.endpoint !== endpoint);
+  saveWebhooks();
+  res.send("Webhook unregistered successfully");
+});
+
+// Save the registered webhooks to a file
+function saveWebhooks() {
+  fs.writeFileSync("./webhooks.json", JSON.stringify(webhooks));
+}
+
+// Event router
+// Events to invoke
+/**
+ * @openapi
+ * /events/trigger-bob:
+ *   get:
+ *     description: Invokes the printBob() event.
+ *     responses:
+ *       200:
+ *         description: Returns confirmation msg.
+ */
+router.get("/trigger-bob", (req, res) => {
+  printBob();
+  res.send("Bob has been triggered");
+});
+
+/**
+ * @openapi
+ * /events/trigger-alice:
+ *   get:
+ *     description: Invokes the printAlice() event.
+ *     responses:
+ *       200:
+ *         description: Returns confirmation msg.
+ */
+router.get("/trigger-alice", (req, res) => {
+  printAlice();
+  res.send("Alice has been triggered");
+});
+
+function printBob() {
+  console.log("Bob");
+  webhooks.forEach((webhook) => {
+    if (webhook.eventType === "bob") {
+      fetch(webhook.endpoint, {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({msg: "Bob has been triggered"}),
+      }).catch((err) => {
+        console.log(err);
+      });
+    }
+  });
+}
+
+function printAlice() {
+  console.log("Alice");
+  webhooks.forEach((webhook) => {
+    if (webhook.eventType === "alice") {
+      fetch(webhook.endpoint, {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({msg: "Alice has been triggered"}),
+      }).catch((err) => {
+        console.log(err);
+      });
+    }
+  });
+}
 
 // Function to call all registered endpoints at random intervals
 function pingEndpoints() {
